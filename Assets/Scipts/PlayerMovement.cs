7bbc7db7cda558c4f62b3bfe4401747ef69beff5
byloosrob <rob.byloos@student.ucll.be>
2023-02-16 11:29:38 +0100

    first person movement

---
+++ Assets/Scipts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float MoveSmoothTime;
+    public float GravityStrength;
+    public float JumpStrength;
+    public float WalkSpeed;
+    public float RunSpeed;
+
+    private CharacterController controller;
+    private Vector3 CurrentMoveVelocity;
+    private Vector3 MoveDampVelocity;
+    private Vector3 CurrentForceVelocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 PlayerInput = new Vector3
+        {
+            x = Input.GetAxis("Horizontal"),
+            y = Of,
+            z = Input.GetAxis("Vertical")
+        };
+
+        if(PlayerInput.magnitude > 1f)
+        {
+            PlayerInput.Normalize();
+        }
+
+        Vector3 MoveVector = transform.TransformDirection(PlayerInput);
+        float CurrentSpeed = Input.getkey(KeyCode.LeftShift) ? RunSpeed : WalkSpeed;
+
+        CurrentMoveVelocity = Vector3.SmoothDamp(
+            CurrentMoveVelocity, 
+            MoveVector * CurrentSpeed, 
+            ref MoveDampVelocity, 
+            MoveSmoothTime
+        );
+
+        controller.Move(CurrentMoveVelocity * Time.deltaTime);
+
+        if(Physics.Raycast(groundCheckRay, 1.1f))
+        {
+            CurrentForceVelocity.y = -2f;
+
+            if(Input.GetKey(KeyCode.Space))
+            {
+                CurrentForceVelocity.y = JumpStrength;
+            }
+            else
+            {
+                CurrentForceVelocity.y -= GravityStrength * Time.deltaTime;
+            }
+
+            Controller.Move(CurrentForceVelocity * Time.deltaTime);
+        }
+    }
+}
+*/
