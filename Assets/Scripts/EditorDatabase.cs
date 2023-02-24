9185c43d20603a48e8eaf15a6030d38d66a61cf7
Sarah Reyskens <sarah.reyskens@student.ucll.be>
2023-02-24 11:03:30 +0100

    add db code

--- Assets/Scripts/EditorDatabase.cs
+++ Assets/Scripts/EditorDatabase.cs
@@ -1,13 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using System.Data;
+using System.Data.SqlClient;
 
 public class EditorDatabase : MonoBehaviour
 {
+
+    private string conn;
+    
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Connecting to database...");
+        conn = @"Data Source=127.0.0.1; user id=SA; password=Password1234; Initial Catalog=testdb;";
+
+        SqlConnection dbconn = new SqlConnection(conn);
+
+        try
+        {
+            dbconn.Open();
+            Debug.Log("Connected to database!");
+        }
+        catch (System.Exception)
+        {
+            Debug.Log("Failed to connect to database!");
+            throw;
+        }
+        finally
+        {
+            dbconn.Close();
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +40,6 @@ public class EditorDatabase : MonoBehaviour
     {
         
     }
+
+    
 }
