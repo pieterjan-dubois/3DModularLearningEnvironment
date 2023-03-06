f0421e3c181b17c948412d66d71b52b3162cf5b1
pieterjan-dubois <55389931+pieterjan-dubois@users.noreply.github.com>
2023-03-06 09:27:57 +0100

    doors

---
+++ Assets/Apartment_Door/Scripts/WrongDoorTrigger.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrongDoorTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      //  respawnPosition = GameObject.FindGameObjectWithTag ("Spawnpoint").transform;
+    }
+/* 
+void OnTriggerEnter (Collider other)
+     {
+    if  (other.gameObject.tag == "WrongDoor")
+         {
+             transform.position = respawnPosition.position;
+         }
+     } */
+}
